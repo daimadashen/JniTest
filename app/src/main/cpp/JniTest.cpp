3408b9179119cccb1b398a674fea8331f5def6d2
zhangfeng <zhangfeng@nanguiyu.com>
2019-07-10 18:23:43 +0800

    新增Jni测试溢出，但是并没有溢出。

--- app/src/main/cpp/JniTest.cpp
+++ app/src/main/cpp/JniTest.cpp
@@ -1,10 +1,18 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_nanguiyu_jnitest_JniTest_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
+
+    jstring jstr;
     std::string hello = "Hello from C++";
+    for(int i = 0;i<100000;i++) {
+        jstr = env->NewStringUTF("Hello from C++");
+        __android_log_print(ANDROID_LOG_DEBUG,"Test","%d",i);
+        env->DeleteLocalRef(jstr);
+    }
     return env->NewStringUTF(hello.c_str());
 }
